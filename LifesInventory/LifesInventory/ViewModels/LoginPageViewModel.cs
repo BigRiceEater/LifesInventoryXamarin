f12303994ba6717c6867ad3de70fe3da18dc22ce
Big-Rice-Eater <34292181+BigRiceEater@users.noreply.github.com>
2018-05-10 13:07:28 +0800

    Add canExecuteMethod stub to LoginCommand

--- LifesInventory/LifesInventory/ViewModels/LoginPageViewModel.cs
+++ LifesInventory/LifesInventory/ViewModels/LoginPageViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace LifesInventory.ViewModels
 {
@@ -16,10 +17,14 @@ namespace LifesInventory.ViewModels
             : base (navigationService)
         {
             Title = "Login Page";
-            LoginCommand = new DelegateCommand(async () =>
+            LoginCommand = new DelegateCommand(executeMethod: async () =>
             {
                 await NavigationService.NavigateAsync("/Root/Nav/Inventory");
-            });
+            },
+            
+            canExecuteMethod: () => true
+            );
+            
         }
     }
 }
