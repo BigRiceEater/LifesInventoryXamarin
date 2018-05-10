d6ccfe686b528ccbbddb858e66282ab5fd8ad92a
Big-Rice-Eater <34292181+BigRiceEater@users.noreply.github.com>
2018-05-10 20:38:54 +0800

    Add category model and service

---
+++ LifesInventory/LifesInventory/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace LifesInventory.Models
+{
+    public class Category
+    {
+        [PrimaryKey]
+        public string Name { get; set; }
+    }
+}
---
+++ LifesInventory/LifesInventory/Services/ICategoryService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using LifesInventory.Models;
+
+namespace LifesInventory.Services
+{
+    public interface ICategoryService
+    {
+        Task<List<Category>> Categories();
+        Task AddCategoryAsync(Category category);
+        Task RemoveCategoryAsync(Category category);
+        Task UpdateCategoryAsync(Category category);
+    }
+}
