dbc4dee45c0b1f0db6cf04958e11355ab6ab83d0
Big-Rice-Eater <34292181+BigRiceEater@users.noreply.github.com>
2018-05-12 13:11:09 +0800

    Add observes property for add inventory command
    Due to limitations in the Prism DelegeteCommand's observe function, only view model properties can be observed i.e. Person.Name is not allowed. And can only observe one property change to auto raise property changed for can execute to be called. So we use ObservesCanExecute on a boolean that is checked when we manually update NewItem instead

---
+++ LifesInventory/LifesInventory/Converters/PriceToFloatConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace LifesInventory.Converters
+{
+    public class PriceToFloatConverter : IValueConverter
+    {
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var number = (float) value;
+                return number.ToString();
+            }
+            else
+                return "";
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            float result = 0.0f;
+            var isString = value is String;
+
+            if (isString)
+            {
+                var asString = value as string;
+                var canConvert = float.TryParse(asString, out result);
+                if (!canConvert) result = 0.0f;
+            }
+
+            return result;
+        }
+    }
+}
--- LifesInventory/LifesInventory/ViewModels/AddInventoryPageViewModel.cs
+++ LifesInventory/LifesInventory/ViewModels/AddInventoryPageViewModel.cs
@@ -3,16 +3,84 @@ using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
+using LifesInventory.Models;
 using Prism.Navigation;
+using Unity.ObjectBuilder.Policies;
 
 namespace LifesInventory.ViewModels
 {
 	public class AddInventoryPageViewModel : ViewModelBase
 	{
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                SetProperty(ref _name, value);
+                UpdateNewItem();
+            }
+        }
+
+	    private string _price;
+	    public string Price
+	    {
+	        get => _price;
+	        set
+	        {
+                SetProperty(ref _price, value);
+	            UpdateNewItem();
+            }
+	    }
+
+	    private bool _hasCompletedEntry = false;
+	    public bool HasCompletedEntry
+	    {
+	        get => _hasCompletedEntry;
+	        set => SetProperty(ref _hasCompletedEntry, value);
+	    }
+
+	    private InventoryAsset _newItem;
+        public InventoryAsset NewItem
+        {
+            get => _newItem;
+            set => SetProperty(ref _newItem, value);
+        }
+
+	    public DelegateCommand<InventoryAsset> AddToInventoryCommand =>
+	        new DelegateCommand<InventoryAsset>
+	            (AddToInventory).ObservesCanExecute(()=>HasCompletedEntry);
+
+	    private void UpdateNewItem()
+	    {
+	        float.TryParse(Price, out var price);
+	        NewItem = new InventoryAsset()
+	        {
+                Name = _name,
+                Price = price
+	        };
+
+	        var hasName = !string.IsNullOrWhiteSpace(NewItem.Name);
+	        var hasPrice = NewItem.Price > 0;
+
+	        if (hasName && hasPrice)
+	            HasCompletedEntry = true;
+	        else
+	            HasCompletedEntry = false;
+	    }
+
         public AddInventoryPageViewModel(INavigationService navigationService) 
             : base(navigationService)
         {
             Title = "Add Inventory";
         }
+
+
+	    private void AddToInventory(InventoryAsset item)
+	    {
+
+	    }
 	}
 }
